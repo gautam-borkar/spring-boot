e6f6a0c8d3afe80c3022148a988f7e0fd7262c65
Gautam Borkar <gborkar@us.ibm.com>
2025-10-17 15:47:55 -0400

    Added save methods for users and roles

--- spring_security/src/main/java/com/gborkar/spring_security/controller/UserController.java
+++ spring_security/src/main/java/com/gborkar/spring_security/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.gborkar.spring_security.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gborkar.spring_security.domain.AppUser;
+import com.gborkar.spring_security.domain.Role;
 import com.gborkar.spring_security.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,4 +28,23 @@ public class UserController {
   public ResponseEntity<List<AppUser>> getUsers() {
     return ResponseEntity.ok().body(userService.getUsers());
   }
+
+  @PostMapping(path = "/users")
+  public ResponseEntity<Long> saveUser(@RequestBody AppUser user) {
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/users").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveUser(user));
+  }
+
+  @PostMapping(path = "/roles")
+  public ResponseEntity<Long> saveRoles(@RequestBody Role role) {
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/roles").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveRole(role));
+  }
+
+  @PostMapping(path = "/users/{username}/roles/{rolename}")
+  public ResponseEntity<?> addRoleToUser(@PathVariable String username, @PathVariable String roleName) {
+    userService.addRoleToUser(username, roleName);
+    return ResponseEntity.ok().build();
+  }
+
 }
