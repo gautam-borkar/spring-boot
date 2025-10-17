887028d43d7476990bdeb6485973de021bcdb3b7
Gautam Borkar <gborkar@us.ibm.com>
2025-10-17 16:09:30 -0400

    Added relationship on role

--- spring_security/src/main/java/com/gborkar/spring_security/domain/AppUser.java
+++ spring_security/src/main/java/com/gborkar/spring_security/domain/AppUser.java
@@ -3,8 +3,10 @@ package com.gborkar.spring_security.domain;
 import java.util.Collection;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,5 +22,7 @@ public class AppUser {
   private String name;
   private String username;
   private String password;
+
+  @ManyToMany(fetch = FetchType.EAGER)
   private Collection<Role> roles;
 }
