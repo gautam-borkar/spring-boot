002b2d4839c4d3818df727449d9a03391bba37e2
Raghoba Gautam Borkar <gautam_borkar@mymail.com>
2025-10-31 15:44:30 -0400

    User post controller

---
+++ spring-aggregator/src/main/java/com/gborkar/spring_aggregator/controller/UserPostController.java
@@ -0,0 +1,34 @@
+package com.gborkar.spring_aggregator.controller;
+
+import java.util.concurrent.ExecutionException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gborkar.spring_aggregator.model.UserPost;
+import com.gborkar.spring_aggregator.service.UserPostAgregatorService;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequiredArgsConstructor
+public class UserPostController {
+
+    private final UserPostAgregatorService userPostAgregatorService;
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Welcome!");
+    }
+
+    @GetMapping("/userpost/{userId}")
+    public ResponseEntity<UserPost> retrieveUserPost(@PathVariable long userId)
+            throws InterruptedException, ExecutionException {
+
+        UserPost userPost = userPostAgregatorService.retrieveUserAndPostByUserIdByVirtualThread(userId);
+        return ResponseEntity.ok(userPost);
+    }
+}
